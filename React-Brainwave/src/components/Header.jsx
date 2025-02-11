68df5e91d09491ef8f0f882703d76e5aea184847
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-11 14:45:15 +0530

    React-Brainwave

--- React-Brainwave/src/components/Header.jsx
+++ React-Brainwave/src/components/Header.jsx
@@ -17,32 +17,32 @@ const Header = () => {
                         alt="Brainwave"
                     />
                 </a>
+                <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+                    <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+                        {navigation.map((item) => (
+                            <a
+                                key={item.id}
+                                href={item.url}
+                                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                                    item.onlyMobile ? "lg:hidden" : ""
+                                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                                    item.url === pathname.hash
+                                        ? "z-2 lg:text-n-1"
+                                        : "lg:text-n-1/50"
+                                } lg:leading5 lg:hover:text-n-1 xl:px-12`}
+                            >
+                                {item.title}
+                            </a>
+                        ))}
+                    </div>
+                </nav>
+                <a
+                    href="#signup"
+                    className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+                >
+                    New account
+                </a>
             </div>
-            <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
-                <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-                    {navigation.map((item) => (
-                        <a
-                            key={item.id}
-                            href={item.url}
-                            className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                                item.onlyMobile ? "lg:hidden" : ""
-                            } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                                item.url === pathname.hash
-                                    ? "z-2 lg:text-n-1"
-                                    : "lg:text-n-1/50"
-                            } lg:leading5 lg:hover:text-n-1 xl:px-12`}
-                        >
-                            {item.title}
-                        </a>
-                    ))}
-                </div>
-            </nav>
-            <a
-                href="#signup"
-                className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
-            >
-                New account
-            </a>
         </div>
     );
 };
