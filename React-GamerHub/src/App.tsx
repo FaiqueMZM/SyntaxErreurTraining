1912594d362b428ed731b296802d7e4413d9d798
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-11 09:45:37 +0530

    React-GamerHub

--- React-GamerHub/src/App.tsx
+++ React-GamerHub/src/App.tsx
@@ -1,15 +1,9 @@
-import ListGroup from "./components/Listgroup";
+import Alert from "./components/Alert";
 
 function App() {
-    let items = ["New York", "San Francisco", "Miami", "London", "Paris"];
-
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    };
-
     return (
         <div>
-            <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+            <Alert>Hello, <span>World!</span></Alert>
         </div>
     );
 }
