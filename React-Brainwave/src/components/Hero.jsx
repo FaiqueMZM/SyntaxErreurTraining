666f123b5cd60246dbc38507534654faf4ee21e2
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-12 18:17:43 +0530

    React-Brainwave

--- React-Brainwave/src/components/Hero.jsx
+++ React-Brainwave/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import Section from "./Section";
+import { curve } from "../assets";
 
 const Hero = () => {
     return (
@@ -9,7 +10,16 @@ const Hero = () => {
             customPaddings
             id="hero"
         >
-            Hero
+            <div className="container realtive">
+                <div className="relative max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+                    <h1 className="h1 mb-6">
+                        Explore the Possibilities of AI Chatting with Brainwave
+                        <span className="inline-block relative">
+                            Brainwave <img src={curve} />
+                        </span>
+                    </h1>
+                </div>
+            </div>
         </Section>
     );
 };
