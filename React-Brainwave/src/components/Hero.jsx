19be68bd280ccb83a0f9fe62aa4bb2c5bb0ccd1f
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-13 09:26:40 +0530

    React-Brainwave

--- React-Brainwave/src/components/Hero.jsx
+++ React-Brainwave/src/components/Hero.jsx
@@ -1,5 +1,7 @@
+import { curve, heroBackground, robot } from "../assets";
+
 import Section from "./Section";
-import { curve } from "../assets";
+import Button from "./Button";
 
 const Hero = () => {
     return (
@@ -15,9 +17,49 @@ const Hero = () => {
                     <h1 className="h1 mb-6">
                         Explore the Possibilities of AI Chatting with Brainwave
                         <span className="inline-block relative">
-                            Brainwave <img src={curve} />
+                            Brainwave{" "}
+                            <img
+                                src={curve}
+                                className="absolute top-full left-0 w-full xl:-mt-2"
+                                width={624}
+                                height={28}
+                                alt="Curve"
+                            />
                         </span>
                     </h1>
+                    <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+                        Unleash the power of AI within Brainwave. Upgrade your
+                        productivity with Brainwave, the open AI chat app.
+                    </p>
+                    <Button href="/pricing" white>
+                        Get Started
+                    </Button>
+                </div>
+                <div className="relative mx-w-[23rem] mx-auto md:mx-w-5xl xl:mb-24">
+                    <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient ">
+                        <div className="relative bg-n-8 rounded-[1rem]">
+                            <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
+                            <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                                <img
+                                    src={robot}
+                                    className="w-full"
+                                    width={1024}
+                                    height={490}
+                                    alt="AI"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+                        <img
+                            src={heroBackground}
+                            className="w-full"
+                            width={1440}
+                            height={800}
+                            alt="hero"
+                        />
+                    </div>
                 </div>
             </div>
         </Section>
