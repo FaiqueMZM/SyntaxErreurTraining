b845fef36bc2327b3f47a236bfca7fce9b63ac02
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-14 09:25:32 +0530

    React-Brainwave

--- React-Brainwave/src/components/Services.jsx
+++ React-Brainwave/src/components/Services.jsx
@@ -18,7 +18,7 @@ const Services = () => {
                     title="Generative AI made for creators."
                     text="Brainwave unlocks the potential of AI-powered applications"
                 />
-                <div className="realtive">
+                <div className="relative">
                     <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
                         <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
                             <img
@@ -45,7 +45,6 @@ const Services = () => {
                                             src={check}
                                             width={24}
                                             height={24}
-                                            alt="check"
                                         />
                                         <p className="ml-4 ">{item}</p>
                                     </li>
@@ -54,7 +53,7 @@ const Services = () => {
                         </div>
                         <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
                     </div>
-                    <div className="realtive z-1 grid gap-5 lg:gridcols2 ">
+                    <div className="relative z-1 grid gap-5 lg:gridcols2 ">
                         <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
                             <div className="absolute inset-0 ">
                                 <img
@@ -113,7 +112,7 @@ const Services = () => {
                                     )}
                                 </ul>
                             </div>
-                            <div className="realtive">
+                            <div className="relative">
                                 <img
                                     src={service3}
                                     className="w-full h-full object-cover"
