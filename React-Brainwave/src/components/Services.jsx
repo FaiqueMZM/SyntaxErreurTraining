510e1c64768a54e875e162465bc4caadb58357de
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-14 09:32:51 +0530

    React-Brainwave

--- React-Brainwave/src/components/Services.jsx
+++ React-Brainwave/src/components/Services.jsx
@@ -46,14 +46,14 @@ const Services = () => {
                                             width={24}
                                             height={24}
                                         />
-                                        <p className="ml-4 ">{item}</p>
+                                        <p className="ml-4">{item}</p>
                                     </li>
                                 ))}
                             </ul>
                         </div>
                         <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
                     </div>
-                    <div className="relative z-1 grid gap-5 lg:gridcols2 ">
+                    <div className="relative z-1 grid gap-5 lg:grid-cols-2">
                         <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
                             <div className="absolute inset-0 ">
                                 <img
@@ -112,7 +112,7 @@ const Services = () => {
                                     )}
                                 </ul>
                             </div>
-                            <div className="relative">
+                            <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem] ">
                                 <img
                                     src={service3}
                                     className="w-full h-full object-cover"
@@ -125,6 +125,7 @@ const Services = () => {
                             </div>
                         </div>
                     </div>
+                    <Gradient />
                 </div>
             </div>
         </Section>
