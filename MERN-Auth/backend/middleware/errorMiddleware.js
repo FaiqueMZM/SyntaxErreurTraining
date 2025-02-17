b09276c64632f46c4add543f077d7c717674c19b
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-17 11:34:36 +0530

    MERN-Auth

--- MERN-Auth/backend/middleware/errorMiddleware.js
+++ MERN-Auth/backend/middleware/errorMiddleware.js
@@ -1,20 +1,20 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found: ${req.originalUrl}`);
-    req.status(404);
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
     next(error);
 };
 
-const errorHandler = (error, req, res, next) => {
-    let statusCode = res.statusCode === 200 ? 500 : req.statusCode;
-    let message = error.message;
+const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
 
-    if (err.name === "CasteError" && err.kind === "ObjectId") {
+    if (err.name === "CastError" && err.kind === "ObjectId") {
         statusCode = 404;
         message = "Resource not found";
     }
 
     res.status(statusCode).json({
-        message,
+        message: message,
         stack: process.env.NODE_ENV === "production" ? null : err.stack,
     });
 };
