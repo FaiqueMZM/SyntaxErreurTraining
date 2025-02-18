27d522a6ace7fb39d766452d581ed1c86b385de9
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-18 12:57:31 +0530

    MERN-Auth

--- MERN-Auth/frontend/src/components/Header.jsx
+++ MERN-Auth/frontend/src/components/Header.jsx
@@ -1,28 +1,33 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 const Header = () => {
     return (
-        <>
-            <header>
-                <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-                    <Container>
-                        <Navbar.Brand href="/">MERN App</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ms-auto">
-                                <Nav.Link href="/login">
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>MERN App</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <LinkContainer to="/login">
+                                <Nav.Link>
                                     <FaSignInAlt /> Sign In
                                 </Nav.Link>
-                                <Nav.Link href="/login">
+                            </LinkContainer>
+                            <LinkContainer to="/register">
+                                <Nav.Link>
                                     <FaSignOutAlt /> Sign Up
                                 </Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </header>
-        </>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     );
 };
 
--- MERN-Auth/frontend/src/main.jsx
+++ MERN-Auth/frontend/src/main.jsx
@@ -1,11 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index={true} path="/" element={<HomeScreen />} />
+        </Route>
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
