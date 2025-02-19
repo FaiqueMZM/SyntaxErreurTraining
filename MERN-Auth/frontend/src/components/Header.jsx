6c80cd0f9ec0571f17549105ed4c2e3baebc4ac4
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-19 10:47:12 +0530

    MERN-Auth

--- MERN-Auth/frontend/src/components/Header.jsx
+++ MERN-Auth/frontend/src/components/Header.jsx
@@ -1,10 +1,27 @@
 import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 
 const Header = () => {
     const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate("/");
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <header>
--- MERN-Auth/frontend/src/slices/usersApiSlice.js
+++ MERN-Auth/frontend/src/slices/usersApiSlice.js
@@ -10,7 +10,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),
         }),
+        logout: builder.mutation({
+            query: (builder) => ({
+                url: `${USERS_URL}/logout`,
+                method: "POST",
+            }),
+        }),
     }),
 });
 
-export const { useLoginMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
