abb0f8b9eea9c1d63c082c33d2d622cd738faa41
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-20 12:52:22 +0530

    REST-API-MERN

--- REST-API-MERN/src/controllers/users.ts
+++ REST-API-MERN/src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { deleteUserById, getUsers, getUserById } from "../db/users";
 
 export const getAllUsers = async (
     req: express.Request,
@@ -13,5 +13,50 @@ export const getAllUsers = async (
     } catch (error) {
         console.log(error);
         res.sendStatus(400);
+    }
+};
+
+export const deleteUser = async (
+    req: express.Request,
+    res: express.Response
+) => {
+    try {
+        const { id } = req.params;
+
+        const deletedUser = await deleteUserById(id);
+        if (!deletedUser) {
+            res.sendStatus(404);
+            return;
+        }
+
+        res.status(200).json(deletedUser);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400);
+    }
+};
+
+export const updateUser = async (
+    req: express.Request,
+    res: express.Response
+) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        if (!username) {
+            res.sendStatus(404);
+            return;
+        }
+
+        const user = await getUserById(id);
+
+        user.username = username;
+        await user.save();
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        return;
     }
 };
--- REST-API-MERN/src/middlewares/index.ts
+++ REST-API-MERN/src/middlewares/index.ts
@@ -3,7 +3,33 @@ import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../db/users";
 
-export const isAuthneticated = async (
+export const isOwner = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, "identity._id") as string;
+
+        if (!currentUserId) {
+            res.sendStatus(403);
+            return;
+        }
+
+        if (currentUserId.toString() !== id) {
+            res.sendStatus(403);
+            return;
+        }
+
+        next();
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(400);
+    }
+};
+
+export const isAuthenticated = async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
