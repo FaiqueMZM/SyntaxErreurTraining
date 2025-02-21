c2b55ffe897bf44c181748d671ea56b849ce8adc
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-21 14:45:01 +0530

    CRUD-Backend-JS

---
+++ CRUD-Backend-JS/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("CRUD JS API");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+const itemRoutes = require("./routes/itemRoutes");
+app.use("/api/items", itemRoutes);
