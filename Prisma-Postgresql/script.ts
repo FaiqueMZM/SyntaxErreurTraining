7d38950ba07944762a1d7bb85a43bae6a91e24bd
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-22 22:25:37 +0530

    Prisma-Postgresql

--- Prisma-Postgresql/script.ts
+++ Prisma-Postgresql/script.ts
@@ -3,6 +3,22 @@ const prisma = new PrismaClient();
 
 async function main() {
     await prisma.user.deleteMany();
+    const users = await prisma.user.createMany({
+        data: [
+            {
+                name: "Harry",
+                email: "harrp@hogwarts.com",
+                age: 18,
+            },
+            {
+                name: "Ron",
+                email: "ronW@hogwarts.com",
+                age: 18,
+            },
+        ],
+    });
+
+    console.log(users);
 }
 
 main()
